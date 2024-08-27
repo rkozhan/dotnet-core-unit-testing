44fc3403a9611e721b87203de53181bdff7852dd
rkozhan~ <yalta.vrk@gmail.com>
2024-08-27 19:21:58 +0200

    InlineData attribute

--- CalculatorApp.Test/MathHelperTest.cs
+++ CalculatorApp.Test/MathHelperTest.cs
@@ -19,5 +19,25 @@ namespace CalculatorApp.Test
             Assert.False(xResult);
             Assert.True(yResult);
         }
+
+        [Theory]
+        [InlineData(1, 2, 1)]
+        [InlineData(1, 3, 2)]
+        public void DiffTest(int x, int y, int expectedValue)
+        {
+            var calculator = new MathFormulas();
+            var result = calculator.Diff(x, y);
+            Assert.Equal(result, expectedValue);
+        }
+
+        [Theory]
+        [InlineData(new int[3] {1 ,2, 3}, 6)]
+        [InlineData(new int[3] { -4, -6, -10}, -20)]
+        public void SumTest(int[] values, int expectedValue)
+        {
+            var calculatot = new MathFormulas();
+            var result = calculatot.Sum(values);
+            Assert.Equal(result, expectedValue);
+        }
     }
 }
